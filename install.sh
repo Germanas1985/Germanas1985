d86c87f7a1ffd80f43a81cce80394f0ed6316ca7
Germanas1985 <51878001+Germanas1985@users.noreply.github.com>
2021-12-12 13:09:23 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
     bash 0-preinstall.sh
     arch-chroot /mnt /root/arch/1-setup.sh
     source /mnt/root/arch/install.conf
-    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/ArchTitus/2-user.sh
+    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/arch/2-user.sh
     arch-chroot /mnt /root/arch/3-post-setup.sh
